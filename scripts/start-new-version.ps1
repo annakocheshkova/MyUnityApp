5bc160a1f903a2dae889693c0d069a3410db8d06
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-11-27 13:21:27 +0300

    Reorganize

--- scripts/start-new-version.ps1
+++
@@ -1,8 +0,0 @@
-# Note: Run this from within the root directory
-
-[CmdletBinding()]
-Param(
-    [string]$Version
-)
-
-.\build.ps1 -Script "version.cake" -Target "StartNewVersion" -ExtraArgs '-NewVersion="$Version"'
